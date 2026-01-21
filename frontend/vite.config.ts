4965fe5ade8eba8a6c905c7415aa35dd639f2279
Łukasz <lukasz.szczesny96@gmail.com>
2026-01-21 23:48:45 +0100

    use import.meta.dirname for path alias

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,4 @@
-import { fileURLToPath, URL } from 'node:url'
+import path from 'node:path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -10,7 +10,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL('./src', import.meta.url)),
+      "@": path.resolve(import.meta.dirname, "./src"),
     },
   },
   server: {
